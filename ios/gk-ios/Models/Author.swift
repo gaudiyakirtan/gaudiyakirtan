bf13892b0b22774356b5446acd112dc0256d7cc5
Aditya Kendre <kendreaditya@meta.com>
2026-02-18 20:02:38 -0800

    Fix iOS build: add explicit init to Author and Topic structs
    SwiftData's @Model macro in the project caused the compiler to
    require explicit id parameter in memberwise initializers. Adding
    explicit init methods excluding id resolves the build errors.

--- ios/gk-ios/Models/Author.swift
+++ ios/gk-ios/Models/Author.swift
@@ -4,4 +4,9 @@ struct Author: Identifiable, Hashable {
     let id = UUID()
     let name: String
     let image: String
+
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
 }
--- ios/gk-ios/Models/Topic.swift
+++ ios/gk-ios/Models/Topic.swift
@@ -3,6 +3,10 @@ import Foundation
 struct Topic: Identifiable, Hashable {
     let id = UUID()
     let name: String
+
+    init(name: String) {
+        self.name = name
+    }
     
     var demoSongCount: Int {
         // Generate a pseudo-random count based on the topic name length
